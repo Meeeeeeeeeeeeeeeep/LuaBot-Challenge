58f941eb4a80fa83e542d29293181a052e5435fa
桜花魅ミープ <96662615+Meeeeeeeeeeeeeeeep@users.noreply.github.com>
2025-11-23 13:40:32 +0900

    Delete MeepBot (Lua) directory

--- MeepBot (Lua)/src/Core/main.lua
+++
@@ -1,9 +0,0 @@
-local Discord = require("discordia")
-local Runtime = require("./Runtime.lua")
-
-local Client = Discord.Client()
-
-Runtime.LoadJsonFiles()
-Runtime.DoCommands()
-Runtime.HandleEvents(Client)
-Runtime.RunBot(Client)
